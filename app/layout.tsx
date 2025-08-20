7befef874776b8eead47346c73309368f4a8f87d
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2025-08-20 21:30:08 +0300

    Initialized project, added assets and styles, installed npm packages

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Poppins, Sora } from "next/font/google";
+
+import "./globals.css";
+
+const poppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+  variable: "--font-poppins",
+});
+
+const sora = Sora({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  variable: "--font-sora",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: {
+    default: "Ethan Smith Portfolio",
+    template: "%s | Ethan Smith Portfolio",
+  },
+  description:
+    "Explore Ethan Smith's professional portfolio showcasing modern web development, creative design, and innovative digital solutions. Discover projects, skills, and achievements crafted with expertise and passion.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.variable} ${sora.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
